95411d3fcb716bda705346ef62cbf47cc9285b06
Ivan G Stewart <71293769+ivanstewart2001@users.noreply.github.com>
2023-11-06 07:54:35 -0500

    Update shell2.c

--- Project 1/shell2.c
+++ Project 1/shell2.c
@@ -16,11 +16,13 @@ char delimiters[] = " \t\r\n";
 extern char **environ;
 pid_t child_pid;
 
+// Signal handler for Ctrl-C (SIGINT)
 void handle_sigint(int sig) {
     printf("\n%s", prompt);
     fflush(stdout);
 }
 
+// Signal handler for process timeout (SIGALRM)
 void handle_sigalrm(int sig) {
     if (child_pid != 0) {
         printf("Child process took too long and is being killed.\n");
@@ -33,6 +35,7 @@ int main() {
     char cmd_bak[MAX_COMMAND_LINE_LEN];
     char *arguments[MAX_COMMAND_LINE_ARGS];
 
+    // Register the signal handlers
     signal(SIGINT, handle_sigint);
     signal(SIGALRM, handle_sigalrm);
 
@@ -54,6 +57,7 @@ int main() {
             return 0;
         }
 
+        // Backup the command line for later use
         strcpy(cmd_bak, command_line);
         char *token = strtok(command_line, delimiters);
         int token_count = 0;
@@ -66,6 +70,7 @@ int main() {
 
         bool background = false;
         if (token_count > 0) {
+            // Check if the command should run in the background
             if (strcmp(arguments[token_count - 1], "&") == 0) {
                 arguments[token_count - 1] = NULL;
                 background = true;
@@ -75,6 +80,7 @@ int main() {
         char *input_file = NULL;
         char *output_file = NULL;
         for (int i = 0; i < token_count; i++) {
+            // Check for input and output redirection symbols
             if (strcmp(arguments[i], "<") == 0) {
                 input_file = arguments[i + 1];
                 arguments[i] = NULL;
@@ -85,35 +91,46 @@ int main() {
         }
 
         if (strcmp(arguments[0], "cd") == 0) {
+            // Handle the 'cd' command to change the working directory
             chdir(arguments[1]);
         } else if (strcmp(arguments[0], "pwd") == 0) {
+            // Handle the 'pwd' command to print the current working directory
             printf("%s\n", cwd);
         } else if (strcmp(arguments[0], "exit") == 0) {
+            // Handle the 'exit' command to terminate the shell
             exit(0);
         } else {
+            // Fork a child process for non-built-in commands
             child_pid = fork();
             if (child_pid == 0) {
+                // Child process
                 if (input_file != NULL) {
+                    // Redirect input from a file
                     int fd = open(input_file, O_RDONLY);
                     dup2(fd, STDIN_FILENO);
                     close(fd);
                 }
                 if (output_file != NULL) {
+                    // Redirect output to a file
                     int fd = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
                     dup2(fd, STDOUT_FILENO);
                     close(fd);
                 }
+                // Execute the specified command
                 execvp(arguments[0], arguments);
                 perror("execvp");
                 exit(1);
             } else if (child_pid > 0) {
+                // Parent process
                 if (!background) {
+                    // Set a timer to terminate the process if it runs too long
                     alarm(10);  // set the alarm to 10 seconds
                     int status;
                     waitpid(child_pid, &status, 0);
                     alarm(0);  // cancel the alarm
                 }
             } else {
+                // Forking failed
                 perror("fork");
                 exit(1);
             }
